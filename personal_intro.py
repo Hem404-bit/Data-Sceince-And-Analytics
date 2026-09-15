c0817fc981c271865dae581ba6164c0fd457a76b
HemanthRNayak <hemanthr169@gmail.com>
2026-09-15 11:20:40 +0530

    Add files via upload

---
+++ personal_intro.py
@@ -0,0 +1,5 @@
+x = input(str("What is your name?: "))
+y = int(input("What is your age?: "))
+z = input(str("What is your favorite Hobby?: "))
+a = f"🎉 Welcome {x}! 🎉 \n you are {y} years old and Your Hobby is {z} "
+print(a.upper())
